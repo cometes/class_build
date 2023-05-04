d5f0addeed623c395bb996cc1a02a8cc955edf5d
cometes <vloslain@gmail.com>
2023-05-04 11:04:01 +0900

    빌드 아이디 통일

--- pages/board/[id]/index.js
+++ pages/board/[id]/index.js
@@ -4,8 +4,13 @@ export default function DetailPage () {
     const router = useRouter()
     return (
         <>
-            <div>안녕하세요, 게시판 정적 페이지 입니다.</div>
+            <div>안녕하세요, 게시판 동적 페이지 입니다.</div>
             <div>{router.query.id}</div>
         </>
     )
 }
+
+// export const getServerSideProps = () => {
+//     // 만약 서버사이드 렌더링을 하는 페이지라면? => out 폴더 생성 불가능, 이런 경우 제외 설정 옵션을 해주어야 함. 
+//     // (next.config.js에서 exportPathMap으로 현재 페이지 제외시키기)
+// }
